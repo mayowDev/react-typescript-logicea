3002697e900e4922a2a63d8a032ed702b86fa7f5
mayowDev <mayowdev@gmail.com>
2023-06-08 09:25:39 -0400

    fixed type issue

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './style.scss'
-function Button({onClick, text, className}) {
+function Button({onClick, text, className}:IButton) {
     return (
         <button className={className} onClick={onClick}>{text}</button>
     );
 }
 
+interface IButton{
+    className:string,
+    text:string,
+    onClick:any
+}
 export default Button;
